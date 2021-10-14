437c4fcab3333dfadddff150db8f858adaec978d
chawchitscm <scm.chawchitsuthwe@gmail.com>
2021-10-14 09:46:43 +0630

    [main] fixed bugs

--- services/edit.php
+++ services/edit.php
@@ -8,11 +8,26 @@
         $id = $_POST['todo_id'];
         $name = $_POST['todo_name'];
         $description = $_POST['description'];
-        $due_date = $_POST['due_date'];
-        $checked = $_POST['checked'];
-        $finished_date = $_POST['finished_date'];
+        
+        if (isset($_POST['checked'])) {
+            $checked = $_POST['checked'];
+        } else {
+            $checked = 0;
+        }
 
-        $query = "UPDATE todo SET name='$name', description='$description', due_date='$due_date', checked='$checked', finished_date='$finished_date' WHERE id=$id";
+        if ($_POST['due_date'] == null) {
+            $due_date = "NULL";
+        } else {
+            $due_date = "'".$_POST['due_date']."'";
+        }
+
+        if ($_POST['finished_date'] == null) {
+            $finished_date = "NULL";
+        } else {
+            $finished_date = "'".$_POST['finished_date']."'";
+        }
+
+        $query = "UPDATE todo SET name='$name', description='$description', due_date=$due_date, checked='$checked', finished_date=$finished_date WHERE id=$id";
         mysqli_query($connection, $query); 
 		$_SESSION['message'] = "To Do Updated.";
 		header('location: ../index.php');
--- services/save.php
+++ services/save.php
@@ -11,9 +11,13 @@
     if (isset($_POST['save'])) {
         $name = $_POST['todo_name'];
         $description = $_POST['description'];
-        $due_date = $_POST['due_date'];
+        if ($_POST['due_date'] == null) {
+            $due_date = "NULL";
+        } else {
+            $due_date = "'".$_POST['due_date']."'";
+        }
 
-        $query = "INSERT INTO todo (name, description, due_date) VALUES ('$name', '$description', '$due_date')";
+        $query = "INSERT INTO todo (name, description, due_date) VALUES ('$name', '$description', $due_date)";
         mysqli_query($connection, $query); 
 		$_SESSION['message'] = "New To Do Added.";
 		header('location: ../index.php');
